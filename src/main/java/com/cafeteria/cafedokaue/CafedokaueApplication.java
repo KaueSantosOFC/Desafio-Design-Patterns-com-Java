e861c6e7e4ff856cf9b968e783e9c8e8d2a6ddba
Kauê Santos <kaues0577@gmail.com>
2025-01-04 13:07:07 -0300

    Arquivos java e pom configurado

---
+++ src/main/java/com/cafeteria/cafedokaue/CafedokaueApplication.java
@@ -0,0 +1,13 @@
+package com.cafeteria.cafedokaue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafedokaueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafedokaueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafeteria/cafedokaue/CafedokaueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeteria.cafedokaue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafedokaueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
